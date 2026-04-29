0444efb62c0c7546c3901ec501b615117156e791
Thiago Tavares <thiagodevtavares@gmail.com>
2026-04-28 22:25:12 -0300

    Redirect /casa-leve to /produtos/casa-leve

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/casa-leve',
+        destination: '/produtos/casa-leve',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
